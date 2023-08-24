7dc0bf1a4a8d67bb9ee31a35d2af8bc6af19e7cb
Antonina Timush <antoninatimush@yandex.ru>
2023-08-25 01:08:33 +0300

    change base_url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -105,7 +105,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://api.timish.nomoredomainsicu.ru",
 });
 
 export default api;
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://api.timish.nomoredomainsicu.ru";
 
 function checkResponse(res) {
   return res.ok
